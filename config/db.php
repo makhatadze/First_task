35286001d2079f98e7c0fc8ad282d2a599458145
makhatadze <vitomaxatadze@gmail.com>
2019-10-28 16:08:15 +0400

    Refactor migrations

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
+    'dsn' => 'mysql:host=localhost;dbname=new',
     'username' => 'root',
     'password' => '1234',
     'charset' => 'utf8',
